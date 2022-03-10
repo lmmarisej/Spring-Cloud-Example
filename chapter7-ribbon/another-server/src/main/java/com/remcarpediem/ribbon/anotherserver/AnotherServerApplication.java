b79381872593bd931048a0c638a4205dd560bb40
lmmarisej <lmmarise@foxmail.com>
2022-03-10 18:45:04 +0800

    add Ribbon 负载均衡

---
+++ chapter7-ribbon/another-server/src/main/java/com/remcarpediem/ribbon/anotherserver/AnotherServerApplication.java
@@ -0,0 +1,13 @@
+package com.remcarpediem.ribbon.anotherserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotherServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnotherServerApplication.class, args);
+    }
+}
+
